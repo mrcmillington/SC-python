626dfc820e1ede457f2e0eb92ffccf86ffea1a81
Mr Millington <millingtonc@lordwandsworth.org>
2022-04-21 11:24:12 +0000

    Agile Task 2 - Adding extra name

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-names= ["Jessica","Alison","Phillipa","Sue","Deborah"]
+extra = input("Enter a name")
+names= ["Jessica","Alison","Phillipa","Sue","Deborah",extra]
 for x in range(0,len(names)):
   print(names[x])
