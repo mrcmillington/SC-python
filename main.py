3d570590014d1ab06ffb0cb38e86fbfcbf4984a0
Mr Millington <millingtonc@lordwandsworth.org>
2022-04-21 11:37:38 +0000

    Agile Task 4 - Add all names + enhanced for loop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-extra = input("Enter a name")
-names= ["Jessica","Alison","Phillipa","Sue","Deborah",extra]
-names.sort()
-for x in range(0,len(names)):
-  print(names[x])
+team=[]
+for x in range(0,7):
+  extra = input("Enter a name : ")
+  team.append(extra)
+print("Teamsheet")
+
+for nm in team:
+  print(nm)
